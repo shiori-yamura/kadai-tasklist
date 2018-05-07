5cd9f3d4af3ad4adec81f33bcd5f611bf86c64e4
EC2 Default User <ec2-user@ip-172-31-21-245.us-east-2.compute.internal>
2018-05-07 08:43:33 +0000

    bootstrap design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,16 +4,24 @@
 
 <h1>タスクの追加ページ</h1>
 
+<div class="row">
+<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-lg-6 col-lg-offset-3">
 {!! Form::model($task, ['route' => 'tasks.store']) !!}
 
+<div class="form-group">
 {!! Form::label('status','状態') !!}
-{!! Form::text('status') !!}
+{!! Form::text('status',null,['class' => 'form-control']) !!}
+</div>
 
+<div class="form-group">
 {!! Form::label('content', 'タスク') !!}
-{!! Form::text('content') !!}
+{!! Form::text('content',null,['class' => 'form-control']) !!}
+</div>
 
 {!! Form::submit('追加') !!}
 
 {!! Form::close() !!}
+</div>
+</div>
 
 @endsection
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -4,16 +4,24 @@
 
 <h1>id : {{ $task->id }}の編集ページ</h1>
 
+<div class="row">
+<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-lg-6 col-lg-offset-3">
 {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
 
+<div class="form-group">
 {!! Form::label('status','状態') !!}
-{!! Form::text('status') !!}
+{!! Form::text('status',null,['class' => 'form-control']) !!}
+</div>
 
+<div class="form-group">
 {!! Form::label('content','タスク:') !!}
-{!! Form::text('content') !!}
+{!! Form::text('content',null,['class' => 'form-control']) !!}
+</div>
 
 {!! Form::submit('更新') !!}
 
 {!! Form::close() !!}
+</div>
+</div>
 
 @endsection
